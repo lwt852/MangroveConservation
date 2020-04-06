1b0d658ce5e7d4d627c58e95556e6b7025a6e016
Dirk Colbry <colbrydi@msu.edu>
2020-04-06 09:11:53 -0400

    Removing envs folder from repository

--- envs/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/gongmimi/CMSE802/MangroveConservation/envs/lib"
-XML2_LIBS="-lxml2 -L/home/gongmimi/CMSE802/MangroveConservation/envs/lib -lz -L/home/gongmimi/CMSE802/MangroveConservation/envs/lib -llzma -lpthread -L/home/gongmimi/CMSE802/MangroveConservation/envs/lib  -lm "
-XML2_INCLUDEDIR="-I/home/gongmimi/CMSE802/MangroveConservation/envs/include/libxml2 -I/home/gongmimi/CMSE802/MangroveConservation/envs/include"
-MODULE_VERSION="xml2-2.9.9"
-
